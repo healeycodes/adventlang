d2e176ad6d57c42e3f62a16275c5b6a5692624d3
Andrew <healeycodes@gmail.com>
2021-12-01 20:49:16 +0000

    Move website/ to docs/

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,2 +1,2 @@
-cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" website/wasm_exec.js
-GOOS=js GOARCH=wasm go build -o website/adventlang.wasm web/run.go
+cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" docs/wasm_exec.js
+GOOS=js GOARCH=wasm go build -o docs/adventlang.wasm web/run.go
